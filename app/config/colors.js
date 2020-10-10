fe508ca69924d3307df52a653c46b3b5a2b83868
Matt <matias_moll@yahoo.com.ar>
2020-10-10 17:13:01 -0300

    Added gesture interactions. ListItem delete action too.

--- app/config/colors.js
+++ app/config/colors.js
@@ -4,5 +4,6 @@ export default{
   black: '#000',
   white: '#fff',
   medium: '#6e6969',
-  light: '#999A9D'
+  light: '#999A9D',
+  danger: '#ff5252'
 }
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { StyleSheet, Text, View, FlatList, } from 'react-native'
 
 import ListItem from '../components/ListItem'
+import ListItemDeleteAction from '../components/ListItemDeleteAction'
 import ListItemSeparator from '../components/ListItemSeparator'
 import Screen from '../components/Screen'
-const messages = [
+const initialMessages = [
   {
     id: 1,
     title: 'T1',
@@ -20,6 +21,14 @@ const messages = [
 ]
 
 export default function MessagesScreen() {
+  
+  const [messages, setMessages] = React.useState(initialMessages);
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const handleDelete = messageToDelete => {
+    setMessages(messages.filter(message => message.id !== messageToDelete.id))
+  }
+
   return (
     <Screen>
       <FlatList 
@@ -30,10 +39,23 @@ export default function MessagesScreen() {
             title={item.title} 
             subTitle={item.description} 
             image={item.image}
-            onPress={() => console.log('Message Selected', item)} 
+            onPress={() => console.log('Message Selected', item)}
+            renderRightActions={() => 
+            <ListItemDeleteAction onPress={() => handleDelete(item)}/>} 
           />
         }
         ItemSeparatorComponent={ () => <ListItemSeparator />}
+        refreshing={refreshing}
+        onRefresh ={() => {
+          setMessages([
+            {
+              id: 2,
+              title: 'T2',
+              description: 'D2',
+              image: require('../assets/mosh.jpg')
+            }
+          ])
+        }}
       />
     </Screen>
 
