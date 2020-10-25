cd90d834c754f83fcdafb3829733e11fb4dbb64b
Matt <matias_moll@yahoo.com.ar>
2020-10-24 23:26:35 -0300

    Adding initial caching POC

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import NetInfo, {useNetInfo} from '@react-native-community/netinfo'
+import AsyncStorage from '@react-native-community/async-storage';
+
+
 import { Button, StyleSheet, Image, Switch, Text, TextInput, View } from 'react-native';
 import Screen from './app/components/Screen'
 import { createStackNavigator } from '@react-navigation/stack'
@@ -10,6 +14,21 @@ import NavigationTheme from './app/navigation/NavigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
 
 export default function App() {
+  const netInfo = useNetInfo()
+
+  const demo = async () => {
+    try{
+      await AsyncStorage.setItem('person', JSON.stringify({id:1}))
+      const value = await AsyncStorage.getItem('person')
+      const person = JSON.parse(value)
+      console.log(person)
+    } catch (error){
+      console.log(error)
+    }
+
+  }
+
+  demo()
 
   return (
     <NavigationContainer theme={NavigationTheme}>
