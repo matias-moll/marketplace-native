53c7d06d7aa950a00d81cd75d70146aaea1c4ccc
Matt <matias_moll@yahoo.com.ar>
2020-10-24 22:42:50 -0300

    Adding animations and more apis methods!

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -6,6 +6,8 @@ import {AppForm, AppFormField, AppFormPicker, SubmitButton} from '../components/
 import AppFormImagePicker from '../components/forms/AppFormImagePicker'
 import Screen from '../components/Screen'
 import useLocation from '../hooks/useLocation'
+import listingsApi from '../api/listings'
+import UploadScreen from './UploadScreen'
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(4).label('Title'),
@@ -21,11 +23,32 @@ const categories = [
   {title:'Camera', value:3}
 ]
 
+
 export default function ListingEditScreen() {
   const location = useLocation()
 
+  const [uploadVisible, setUploadVisible] = React.useState(false)
+  const [progress, setProgress] = React.useState(0)
+
+  const handleSubmit = async (listing, {resetForm}) => {
+    setProgress(0)
+    setUploadVisible(true)
+    const result = await listingsApi.addListing(
+      {...listing, location},
+      (progress) => setProgress(progress)
+    )
+
+    if(!result.ok) {
+      setUploadVisible(false)
+      return alert('Could not save the listing.')
+    }
+
+    resetForm();
+  }
+
   return (
     <Screen style={styles.container}>
+      <UploadScreen onDone={() => setUploadVisible(false)} progress={progress} visible={uploadVisible} />
       <AppForm
         initialValues={{
           title:'',
@@ -34,7 +57,7 @@ export default function ListingEditScreen() {
           category: null,
           images: []
         }}
-        onSubmit={(values) => console.log('Values: ' + JSON.stringify(values), 'Location: ' + JSON.stringify(location))}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
         <AppFormImagePicker name='images' />
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,26 +1,48 @@
 import React from 'react'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native'
 
 import Routes from '../navigation/Routes'
 import Screen from '../components/Screen'
 import Card from '../components/Card'
 import listingsApi from '../api/listings'
 import colors from '../config/colors'
+import AppText from '../components/AppText'
+import AppButton from '../components/AppButton'
+import ActivityIndicator from '../components/ActivityIndicator'
 
 export default function ListingsScreen({navigation}) {
   const [listings, setListings] = React.useState([])
+
+  const[error, setError] = React.useState(false)
+
+  const [loading, setLoading] = React.useState(false)
 
   React.useEffect(() => {
     loadListings()
   }, [])
 
   const loadListings = async () => {
+    setLoading(true)
     const response = await listingsApi.getListings()
+    setLoading(false)
+
+    if(!response.ok){
+      setError(true); // response.problem has details.
+      return;
+    }  
+
+    setError(false)
     setListings(response.data)
   }
 
   return (
     <Screen style={styles.screen}>
+      {error && 
+        <> 
+          <AppText> Couldn't retrieve the listings.</AppText>
+          <AppButton title='Retry' onPress={loadListings} /> 
+        </>}
+      <ActivityIndicator visible={loading} />
       <FlatList 
         data={listings}
         keyExtractor= { listing => listing.id.toString()}
